01484047d6b5ac158f7a307ade3f77b39e0d26b2
am20181012 <andjelamijatovic98@gmail.com>
2022-06-24 09:52:26 +0200

    pripremljeni templejti za login i registraciju korisnika

---
+++ pages/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../style/loginstyle.css">
+</head>
+
+<body>
+    <section>
+        <div class="imageBox">
+            <img src="../image/budget.jpg">
+        </div>
+        <div class="contentBox">
+            <div class="formBox">
+                <h2>login</h2>
+                <form>
+                    <div class="inputBox">
+                        <span>Korisnicko ime</span>
+                        <input type="text" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <span>Lozinka</span>
+                        <input type="password" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" value="Prijavi se" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <p>Nemate nalog? <a href="./registration.php">Registrujte se</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ pages/registration.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../style/loginstyle.css">
+</head>
+
+<body>
+    <section>
+        <div class="imageBox">
+            <img src="../image/budget.jpg">
+        </div>
+        <div class="contentBox">
+            <div class="formBox">
+                <h2>registracija</h2>
+                <form>
+                    <div class="inputBox">
+                        <span>Email</span>
+                        <input type="email" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <span>Korisnicko ime</span>
+                        <input type="text" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <span>Lozinka</span>
+                        <input type="password" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <span>Potvrdi lozinku</span>
+                        <input type="password" name="" />
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" value="Registruj se" name="" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
